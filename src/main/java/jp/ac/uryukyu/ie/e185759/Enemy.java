a44fedeba781b3ee15670b66f5633aa40a81e8d1
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2018-12-10 21:39:22 +0900

    report5

---
+++ src/main/java/jp/ac/uryukyu/ie/e185759/Enemy.java
@@ -0,0 +1,29 @@
+package jp.ac.uryukyu.ie.e185759;
+
+public class Enemy extends LivingThing {
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        damageVal(damage);
+        if(damageVal(damage) < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+
+}
